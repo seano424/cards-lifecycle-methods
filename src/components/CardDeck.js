eae6922daaf1d52ba69a651738b89902e1c9b712
Sean OReilly <soreilly424@gmail.com>
2021-04-05 18:29:45 -0500

    finished

--- src/components/CardDeck.js
+++ src/components/CardDeck.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import './CardDeck.css'
 const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
 
 export default class CardDeck extends Component {
@@ -7,7 +8,7 @@ export default class CardDeck extends Component {
     super(props);
     this.state = {
       cards: [],
-      deck: "",
+      deck: ""
     };
     this.handleRequestClick = this.handleRequestClick.bind(this);
   }
@@ -25,20 +26,22 @@ export default class CardDeck extends Component {
     console.log(id);
     const response = await axios.get(`${API_BASE_URL}/${id}/draw/`);
     // console.log(response.data.cards);
-    const card = response.data.cards;
+    const card = {...response.data.cards, randNum: Math.ceil(Math.random() * 45) * (Math.round(Math.random()) ? 1 : -1)};
     this.setState((prevState) => ({
       cards: [...prevState.cards, card],
+      randomNumber: Math.ceil(Math.random() * 45) * (Math.round(Math.random()) ? 1 : -1)
     }));
   }
 
   render() {
     return (
-      <div>
+      <div className="CardDeck">
         <h1 onClick={this.handleRequestClick}>Gimme a card!</h1>
-        <div>
+        <div className="cards">
           {this.state.cards !== [] &&
             this.state.cards.map((card) => (
               <img
+                style={{transform: `rotate(${card.randNum}deg)`}}
                 src={card[0].image}
                 alt={`${card[0].value} of ${card[0].suit}`}
               />
